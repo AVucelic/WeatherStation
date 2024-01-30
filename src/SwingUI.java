83af29844ecf209f4e6e33494b9c553151ec9ba5
jleskovac <jl8592@rit.edu>
2024-01-30 17:11:20 +0100

    Added the setJLabel method

--- src/SwingUI.java
+++ src/SwingUI.java
@@ -14,123 +14,128 @@
  * a Swing application.
  */
 
- import java.awt.Font;
- import java.awt.GridLayout;
- import java.util.EnumMap;
- import java.util.Map;
- 
- import javax.swing.JFrame;
- import javax.swing.JLabel;
- import javax.swing.JPanel;
- 
- //import java.text.DecimalFormat ;
- 
- public class SwingUI extends JFrame {
-     private JLabel celsiusField; // put current celsius reading here
-     private JLabel kelvinField; // put current kelvin reading here
- 
-     /*
-      * A Font object contains information on the font to be used to
-      * render text.
-      */
-     private static Font labelFont = new Font(Font.SERIF, Font.PLAIN, 72);
- 
-     /*
-      * Create and populate the SwingUI JFrame with panels and labels to
-      * show the temperatures.
-      */
-     public SwingUI() {
-         super("Weather Station");
- 
-         /*
-          * WeatherStation frame is a grid of 1 row by an indefinite
-          * number of columns.
-          */
-         this.setLayout(new GridLayout(1, 0));
- 
-         /*
-          * There are two panels, one each for Kelvin and Celsius, added to the
-          * frame. Each Panel is a 2 row by 1 column grid, with the temperature
-          * name in the first row and the temperature itself in the second row.
-          */
-         // JPanel panel;
- 
-         /*
-          * Set up Kelvin display.
-          */
-         // panel = new JPanel(new GridLayout(2, 1));
-         // this.add(panel);
-         // createLabel(" Kelvin ", panel);
-         // kelvinField = createLabel("", panel);
- 
-         // /*
-         // * Set up Celsius display.
-         // */
-         // panel = new JPanel(new GridLayout(2, 1));
-         // this.add(panel);
-         // createLabel(" Celsius ", panel);
-         // celsiusField = createLabel("", panel);
- 
-         /**
-          * For loop to iterate through all units and values of units, in a way that it is not hardcoded
-          */
-         JPanel panel = null;
-         for (TemperatureUnit unit : TemperatureUnit.values()) {
-             panel = createPanel(unit);
-             this.add(panel);
-         }
-         /*
-          * Set up the frame's default close operation pack its elements,
-          * and make the frame visible.
-          */
-         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         this.pack();
-         this.setVisible(true);
-     }
- 
-     /*
-      * Set the label holding the Kelvin temperature.
-      */
-     public void setKelvinJLabel(double temperature) {
-         kelvinField.setText(String.format("%6.2f", temperature));
-     }
- 
-     // /*
-     // * Set the label holding the Celsius temperature.
-     // */
-     public void setCelsiusJLabel(double temperature) {
-         celsiusField.setText(String.format("%6.2f", temperature));
-     }
- 
-     /**
-      * Method to create panels without hard-coding
-      * @param unit - gets the kind of a unit
-      * @return panel
-      */
-     private JPanel createPanel(TemperatureUnit unit) {
-         JPanel panel = new JPanel(new GridLayout(2, 1));
-         createLabel(unit.name(), panel);
-         return panel;
-     }
- 
-     /*
-      * Create a Label with the initial value <title>, place it in
-      * the specified <panel>, and return a reference to the Label
-      * in case the caller wants to remember it.
-      */
-     private JLabel createLabel(String title, JPanel panel) {
-         JLabel label = new JLabel(title);
-         label.setHorizontalAlignment(JLabel.CENTER);
-         label.setVerticalAlignment(JLabel.TOP);
-         label.setFont(labelFont);
-         panel.add(label);
- 
-         return label;
-     }
- 
-     public static void main(String[] args) {
-         SwingUI test = new SwingUI();
- 
-     }
- }
- 
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.EnumMap;
+import java.util.Map;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+//import java.text.DecimalFormat ;
+
+public class SwingUI extends JFrame {
+    private JLabel celsiusField; // put current celsius reading here
+    private JLabel kelvinField; // put current kelvin reading here
+
+    /*
+     * A Font object contains information on the font to be used to
+     * render text.
+     */
+    private static Font labelFont = new Font(Font.SERIF, Font.PLAIN, 72);
+
+    /*
+     * Create and populate the SwingUI JFrame with panels and labels to
+     * show the temperatures.
+     */
+    public SwingUI() {
+        super("Weather Station");
+
+        /*
+         * WeatherStation frame is a grid of 1 row by an indefinite
+         * number of columns.
+         */
+        this.setLayout(new GridLayout(1, 0));
+
+        /*
+         * There are two panels, one each for Kelvin and Celsius, added to the
+         * frame. Each Panel is a 2 row by 1 column grid, with the temperature
+         * name in the first row and the temperature itself in the second row.
+         */
+        // JPanel panel;
+
+        /*
+         * Set up Kelvin display.
+         */
+        // panel = new JPanel(new GridLayout(2, 1));
+        // this.add(panel);
+        // createLabel(" Kelvin ", panel);
+        // kelvinField = createLabel("", panel);
+
+        // /*
+        // * Set up Celsius display.
+        // */
+        // panel = new JPanel(new GridLayout(2, 1));
+        // this.add(panel);
+        // createLabel(" Celsius ", panel);
+        // celsiusField = createLabel("", panel);
+
+        /**
+         * For loop to iterate through all units and values of units, in a way that it
+         * is not hardcoded
+         */
+        JPanel panel = null;
+        for (TemperatureUnit unit : TemperatureUnit.values()) {
+            panel = createPanel(unit);
+            this.add(panel);
+        }
+        /*
+         * Set up the frame's default close operation pack its elements,
+         * and make the frame visible.
+         */
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    /*
+     * Set the label holding the Kelvin temperature.
+     */
+    // public void setKelvinJLabel(double temperature) {
+    // kelvinField.setText(String.format("%6.2f", temperature));
+    // }
+
+    // // /*
+    // // * Set the label holding the Celsius temperature.
+    // // */
+    // public void setCelsiusJLabel(double temperature) {
+    // celsiusField.setText(String.format("%6.2f", temperature));
+    // }
+
+    private void setJLable(TemperatureUnit unit, double value) {
+        
+    }
+
+    /**
+     * Method to create panels without hard-coding
+     * 
+     * @param unit - gets the kind of a unit
+     * @return panel
+     */
+    private JPanel createPanel(TemperatureUnit unit) {
+        JPanel panel = new JPanel(new GridLayout(2, 1));
+        createLabel(unit.name(), panel);
+        return panel;
+    }
+
+    /*
+     * Create a Label with the initial value <title>, place it in
+     * the specified <panel>, and return a reference to the Label
+     * in case the caller wants to remember it.
+     */
+    private JLabel createLabel(String title, JPanel panel) {
+        JLabel label = new JLabel(title);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setFont(labelFont);
+        panel.add(label);
+
+        return label;
+    }
+
+    public static void main(String[] args) {
+        SwingUI test = new SwingUI();
+
+    }
+}
