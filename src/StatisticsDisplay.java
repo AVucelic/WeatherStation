0d923832e6c6e6f9938deb41265cfe8d8d2850b1
jl8592 <jl8592@rit.edu>
2024-02-16 15:59:56 +0100

    Creating StatisticsDisplay class

---
+++ src/StatisticsDisplay.java
@@ -0,0 +1,18 @@
+public class StatisticsDisplay implements Observer {
+
+    private WeatherStation ws;
+
+    public StatisticsDisplay(WeatherStation ws) {
+        this.ws = ws;
+
+        //TODO - add to observer arraylist
+    }
+
+
+    //todo method
+    @Override
+    public void update() {
+
+    }
+
+}
