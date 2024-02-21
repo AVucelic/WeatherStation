0e9789030a81589253e9b8c4b8074187187124e7
Ejup Elezi <ee1641@rit.edu>
2024-02-21 16:59:37 +0100

    Implemented the get method for sensorFactory, which takes sensorType as a parameter and a new sensor of that type

--- src/edu/rit/croatia/swen383/g1/ws/factory/SensorFactory.java
+++ src/edu/rit/croatia/swen383/g1/ws/factory/SensorFactory.java
@@ -1,5 +1,28 @@
 package edu.rit.croatia.swen383.g1.ws.factory;
 
+import edu.rit.croatia.swen383.g1.ws.sensor.PressureSensor;
+import edu.rit.croatia.swen383.g1.ws.sensor.Sensor;
+import edu.rit.croatia.swen383.g1.ws.sensor.TemperatureSensor;
+import edu.rit.croatia.swen383.g1.ws.util.SensorType;
+
 public class SensorFactory {
 
+    public SensorFactory() {
+
+    }
+
+    public Sensor get(SensorType sensorType) {
+        Sensor sensor;
+        switch (sensorType) {
+            case TEMPERATURE:
+                sensor = new TemperatureSensor();
+                break;
+            case PRESSURE:
+                sensor = new PressureSensor();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported sensor type: " + sensorType);
+        }
+        return sensor;
+    }
 }
